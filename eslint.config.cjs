a934c37b32e0507dd9a4cfd58b369091bae7175b
HexaField <10372036+HexaField@users.noreply.github.com>
2025-10-14 15:39:03 +1100

    fix: update ESLint rule for semicolons to enforce no semicolons

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -17,7 +17,7 @@ module.exports = [
       '@typescript-eslint': tseslint
     },
     rules: {
-      semi: ['error', 'always'],
+      semi: 'never',
       quotes: ['error', 'single'],
       '@typescript-eslint/no-unused-vars': ['warn'],
       '@typescript-eslint/explicit-function-return-type': ['off']
